51a9e1e21c5c0f21ea3a367d878da33235bd6f82
dej611 <dej611@gmail.com>
2018-11-20 19:16:58 +0100

    :sparkles: Basic apollo

--- dashboard/src/ChartContainer.js
+++ dashboard/src/ChartContainer.js
@@ -1,8 +1,6 @@
 import React, {PureComponent} from 'react';
-
 import {Query} from 'react-apollo';
 import gql from 'graphql-tag';
-import Chart from './Chart';
 
 const ALL_TAGS = gql`
     query {
@@ -15,12 +13,12 @@ const ALL_TAGS = gql`
             }
         }
     }
-`;
+`
 
-class TagChart extends PureComponent{
+export default class ChartContainer extends PureComponent{
     render(){
         return <div>
-            Meetup Tags
+            Chart container
             <Query query={ALL_TAGS}>
                 {({loading, error, data}) => {
                     if(loading){
@@ -29,46 +27,13 @@ class TagChart extends PureComponent{
                     if(error){
                         return <p>Error</p>
                     }
-                    return <Chart data={data.allTags} label='name' size={tag => tag.meetups.length}/>;
+                    return <div>
+                        {data.allTags.map( (tag) => {
+                            return <p>[{tag.id}] - {tag.name} => {tag.meetups.length} meetups</p>
+                        })}
+                    </div>
                 }}
             </Query>
         </div>;
     }
 };
-
-const ALL_MEETUPS = gql`
-    query {
-        allMeetups {
-        id
-        name
-        tags {
-            name
-            }
-        }
-    }
-`;
-
-class MeetupChart extends PureComponent{
-    render(){
-        return <div>
-            Meetups
-            <Query query={ALL_MEETUPS}>
-                {({loading, error, data}) => {
-                    if(loading){
-                        return <p>Loading...</p>
-                    }
-                    if(error){
-                        return <p>Error</p>
-                    }
-                    return <Chart data={data.allMeetups} label='name' size={meetup => meetup.tags.length}/>;
-                }}
-            </Query>
-        </div>;
-    }
-};
-
-export default class ChartsContainer extends PureComponent{
-    render(){
-        return <div style={{display: 'flex', flexDirection: 'row'}}><MeetupChart /><TagChart /></div>
-    }
-}
